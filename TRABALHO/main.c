257567b2315a7eab8771f3dcb3d42a04b7d40cef
WKesneY <wildneysousa25@gmail.com>
2024-04-05 09:08:16 -0300

    Alterações código main

--- TRABALHO/main.c
+++ TRABALHO/main.c
@@ -20,28 +20,28 @@ int main(){
 
         switch (lexema) {
             case 1:
-                printf("TOKEN: [ID]                        | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [ID]                         | LEXEMA: %s\n", palavra);
                 break;
             case 2:
-                printf("TOKEN: [OPERADOR DE SOMA]          | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [OPERADOR_SOM]               | LEXEMA: %s\n", palavra);
                 break;
             case 3:
-                printf("TOKEN: [OPERADOR DE SUBTRACAO]     | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [OPERADOR_SUB]               | LEXEMA: %s\n", palavra);
                 break;
             case 4:
-                printf("TOKEN: [OPERADOR DE DIVISAO]       | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [OPERADOR_DIV]               | LEXEMA: %s\n", palavra);
                 break;
             case 5:
-                printf("TOKEN: [OPERADOR DE MULTIPLICACAO] | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [OPERADOR_MUL]               | LEXEMA: %s\n", palavra);
                 break;
             case 6:
-                printf("TOKEN: [INTEIRO]                   | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [INTEIRO]                    | LEXEMA: %s\n", palavra);
                 break;
             case 7:
-                printf("TOKEN: [FLOAT]                     | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [FLOAT]                      | LEXEMA: %s\n", palavra);
                 break;
             default:
-                printf("TOKEN: [ERRO]                      | LEXEMA: %s\n", palavra);
+                printf("TOKEN: [ERRO]                       | LEXEMA: %s\n", palavra);
                 break;
         }
 
